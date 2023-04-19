29da3f693ad5a5b09db04d0b2957b64a2e3b5ee7
ASCANO Jayvee <91439919+jaybee291998@users.noreply.github.com>
2023-04-19 09:48:39 +0800

    initial commit

---
+++ src/main/java/com/cwallet/CHAMPWallet/ChampWalletApplication.java
@@ -0,0 +1,13 @@
+package com.cwallet.CHAMPWallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChampWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cwallet/CHAMPWallet/ChampWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwallet.CHAMPWallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChampWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

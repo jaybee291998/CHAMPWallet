fada09b474ac64963776b19741a7afdc4372ef5d
jaybee291998 <jaybee291998@gmail.com>
2023-05-07 18:11:32 +0800

    added boilerplate stuff

---
+++ src/main/java/com/cwallet/CHAMPWallet/service/impl/budget/BudgetServiceImpl.java
@@ -0,0 +1,16 @@
+package com.cwallet.CHAMPWallet.service.impl.budget;
+
+import com.cwallet.CHAMPWallet.dto.budget.BudgetDTO;
+import com.cwallet.CHAMPWallet.repository.budget.BudgetRepository;
+import com.cwallet.CHAMPWallet.service.budget.BudgetService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class BudgetServiceImpl implements BudgetService {
+    @Autowired
+    private BudgetRepository budgetRepository;
+
+    @Override
+    public boolean save(BudgetDTO budgetDTO) {
+        return false;
+    }
+}

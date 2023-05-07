7022bc64fd13bbe1db9252cb17324a342c218cef
jaybee291998 <jaybee291998@gmail.com>
2023-05-07 18:23:30 +0800

    added more boilerplate

--- src/main/java/com/cwallet/CHAMPWallet/controller/budget/BudgetController.java
+++ src/main/java/com/cwallet/CHAMPWallet/controller/budget/BudgetController.java
@@ -1,4 +1,20 @@
 package com.cwallet.CHAMPWallet.controller.budget;
 
+import com.cwallet.CHAMPWallet.bean.budget.BudgetForm;
+import com.cwallet.CHAMPWallet.service.budget.BudgetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BudgetController {
+    @Autowired
+    private BudgetService budgetService;
+
+    @GetMapping("/users/budget/create")
+    public String getBudgetForm(Model model) {
+        model.addAttribute("budgetForm", new BudgetForm());
+        return "budget/budget-creation";
+    }
 }

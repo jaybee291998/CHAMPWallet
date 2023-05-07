eefa5ead3038385bb4633328692ca796115da31b
FloresEdward <119044984+FloresEdward@users.noreply.github.com>
2023-05-07 15:37:11 +0800

    created the functionality of expense type for creating form

--- src/main/java/com/cwallet/CHAMPWallet/controller/expenseType/ExpenseTypeController.java
+++ src/main/java/com/cwallet/CHAMPWallet/controller/expenseType/ExpenseTypeController.java
@@ -1,9 +1,19 @@
 package com.cwallet.CHAMPWallet.controller.expenseType;
 
+import com.cwallet.CHAMPWallet.bean.expenseType.ExpenseTypeForm;
+import com.cwallet.CHAMPWallet.dto.expenseType.ExpenseTypeDto;
+import com.cwallet.CHAMPWallet.models.expenseType.ExpenseType;
 import com.cwallet.CHAMPWallet.service.expenseType.ExpenseTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.time.Clock;
 
 @Controller
 public class ExpenseTypeController {
@@ -14,6 +24,26 @@ public class ExpenseTypeController {
         this.expenseTypeService = expenseTypeService;
     }
 
-    @GetMapping("/expenseType")
-    public String
+    @GetMapping("/users/expense-type/create")
+    public String getExpenseTypeForm(Model model){
+        model.addAttribute("expenseTypeForm", new ExpenseTypeForm());
+        return "create-expense-type-form";
+    }
+
+    @PostMapping("/users/expense-type/create")
+    public String createExpenseTypeForm(@Valid @ModelAttribute("expenseTypeForm") ExpenseTypeForm expenseTypeForm,
+                                        BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            System.out.println(expenseTypeForm);
+            model.addAttribute("expenseTypeForm", expenseTypeForm);
+            return "create-expense-type-form";
+        }
+        ExpenseTypeDto newExpense = ExpenseTypeDto.builder()
+                .name((expenseTypeForm.getName()))
+                .description(expenseTypeForm.getDescription())
+                .build();
+        expenseTypeService.save(newExpense);
+
+        return "redirect:/users/home";
+    }
 }
--- src/main/java/com/cwallet/CHAMPWallet/service/expenseType/impl/ExpenseTypeServiceImpl.java
+++ src/main/java/com/cwallet/CHAMPWallet/service/expenseType/impl/ExpenseTypeServiceImpl.java
@@ -1,8 +1,11 @@
 package com.cwallet.CHAMPWallet.service.expenseType.impl;
 
 import com.cwallet.CHAMPWallet.dto.expenseType.ExpenseTypeDto;
+import com.cwallet.CHAMPWallet.models.account.UserEntity;
+import com.cwallet.CHAMPWallet.models.account.Wallet;
 import com.cwallet.CHAMPWallet.models.expenseType.ExpenseType;
 import com.cwallet.CHAMPWallet.repository.expenseType.ExpenseTypeRepository;
+import com.cwallet.CHAMPWallet.security.SecurityUtil;
 import com.cwallet.CHAMPWallet.service.expenseType.ExpenseTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,15 +15,24 @@ import static com.cwallet.CHAMPWallet.mappers.expenseType.ExpenseTypeMapper.mapT
 public class ExpenseTypeServiceImpl implements ExpenseTypeService {
 
     private ExpenseTypeRepository expenseTypeRepository;
+    private SecurityUtil securityUtil;
 
     @Autowired
-    public ExpenseTypeServiceImpl(ExpenseTypeRepository expenseTypeRepository){
+    public ExpenseTypeServiceImpl(ExpenseTypeRepository expenseTypeRepository, SecurityUtil securityUtil){
         this.expenseTypeRepository = expenseTypeRepository;
+        this.securityUtil = securityUtil;
     }
     @Override
     public void save(ExpenseTypeDto expenseTypeDto) {
+        UserEntity loggedInUser = securityUtil.getLoggedInUser();
+        Wallet newWallet = loggedInUser.getWallet();
+
         ExpenseType expense = mapToExpenseType(expenseTypeDto);
-        ExpenseType newExpense = expenseTypeRepository.save(expense);
+        expense.setWallet(newWallet);
+        expense.setEnabled(true);
+        expenseTypeRepository.save(expense);
+
+
 
     }
 }

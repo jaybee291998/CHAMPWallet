cce4e4a086ba94027ec30d6ffb366d5bdd4fa21b
jaybee291998 <jaybee291998@gmail.com>
2023-05-09 14:51:57 +0800

    added utilities for expiry and ownership, also restricted logged in user from going to login and registration page

--- src/main/java/com/cwallet/CHAMPWallet/models/expense/Expense.java
+++ src/main/java/com/cwallet/CHAMPWallet/models/expense/Expense.java
@@ -2,11 +2,19 @@ package com.cwallet.CHAMPWallet.models.expense;
 
 import com.cwallet.CHAMPWallet.models.account.Wallet;
 import com.cwallet.CHAMPWallet.models.budget.Budget;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "expense")
+@Builder
 public class Expense {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)

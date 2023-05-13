eb4a17f29c2b9003bcf6f1ef366a8a60b6ef223b
Calics <junpiocalicdan@gmail.com>
2023-05-13 13:29:39 +0800

    Delete hamcrest for dependency

--- src/test/java/com/cwallet/champwallet/bean/income/IncomeFormTest.java
+++ src/test/java/com/cwallet/champwallet/bean/income/IncomeFormTest.java
@@ -6,8 +6,6 @@
 //import org.junit.jupiter.api.Test;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
-//import static org.hamcrest.Matchers.is;
-//import static org.hamcrest.MatcherAssert.assertThat;
 //import static org.junit.Assert.*;
 //import static org.junit.matchers.JUnitMatchers.*;
 //
